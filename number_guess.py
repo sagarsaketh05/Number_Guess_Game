3b8d1f48a3243e29f4e9805477c910950fcd4ce5
sagarsaketh05 <146060127+sagarsaketh05@users.noreply.github.com>
2025-07-07 23:36:37 +0530

    Add files via upload

---
+++ number_guess.py
@@ -0,0 +1,15 @@
+import random
+x=True
+number=random.randint(1,100)
+while x:
+    try:
+        guess=int(input("Enter a Number Between 1 to 100 : "))
+        if guess>number:
+            print("greater")
+        elif guess < number :
+            print("lower")    
+        else:
+            print("you guessed it correctly!")   
+            break
+    except ValueError:
+        print("ivalid choice!")    
